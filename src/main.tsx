8bea49ea1ec592cd6c37da09cfb04891802d14bf
Дмитрий Егоров <egorov.dima1470@gmail.com>
2025-11-12 11:54:38 +0300

    https

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {registerSW} from "virtual:pwa-register";
 import './index.css'
 import App from './App.tsx'
+
+if ("serviceWorker" in navigator) {
+  registerSW()
+}
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,51 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+import mkcert from 'vite-plugin-mkcert'
+import fs from 'fs';
+import path from 'path';
 
 const isDocker = process.env.DOCKER === 'true';
 const apiHost = isDocker ? 'http://host.docker.internal' : 'http://localhost';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    mkcert(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest:{
+        name: "History",
+        short_name: "History",
+        start_url: "/iu5_web_front/",
+        display: "standalone",
+        background_color: "#fdfdfd",
+        theme_color: "#db4938",
+        orientation: "portrait-primary",
+        icons: [
+          {
+            src: "flask-icon-180.png",
+            type: "image/png",
+            sizes: "180x180",
+            purpose: "any maskable"
+          },
+          {
+            src: "flask-icon-512.png",
+            type: "image/png",
+            sizes: "512x512"
+          },
+          {
+            src: "flask-icon-192.png",
+            type: "image/png",
+            sizes: "192x192"
+          }
+        ],
+      }
+    })
+  ],
   base: '/iu5_web_front/',
   server: {
     proxy: {
@@ -15,6 +55,10 @@ export default defineConfig({
         // УБРАТЬ rewrite - бэкенд должен получать /api/anomalies
         // rewrite: (path) => path.replace(/^\/api/, "/"),
       },
+    },
+    https:{
+      key: fs.readFileSync(path.resolve(__dirname, 'cert.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'cert.crt')),
     },
     watch: {
         usePolling: true,
