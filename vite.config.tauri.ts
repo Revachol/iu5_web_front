fe3801ee41ecf1f52c28fcff688ac72373e5e830
Revachol <egorov.dima1470@gmail.com>
2025-12-07 11:27:44 +0300

    tauri test

--- vite.config.tauri.ts
+++ vite.config.tauri.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // Относительные пути для Tauri
   server: {
     port: 1420,
     strictPort: true,
